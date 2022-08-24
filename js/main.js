35fe3872689427de8c700f81ef2d7d689520d5ee
r69ert <frontend.robert@gmail.com>
2022-08-24 23:46:55 +0300

    fix bug

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 const searchInput = document.querySelector(".header-search__input");
 const searchBlock = document.querySelector(".search-block");
+const header = document.querySelector(".header");
 const headerModal = document.querySelector(".header-modal");
 const headerBack = document.querySelector(".header-block__back");
+const headerLogo = document.querySelector(".header-block__logo");
 const body = document.querySelector("body");
 
 const mediaQuery = window.matchMedia('(min-width: 768px)')
@@ -22,11 +24,17 @@ function handleTabletChange(e) {
 }
 
 searchInput.addEventListener('click', function (event) {
+	header.classList.add('header-modal__sticky');
 	headerModal.classList.add('active');
+	headerBack.classList.add('active');
+	headerLogo.style.display = "none";
 	body.classList.add('lock');
 });
 headerBack.addEventListener('click', function (event) {
+	header.classList.remove('header-modal__sticky');
 	headerModal.classList.remove('active');
+	headerBack.classList.remove('active');
+	headerLogo.style.display = "block";
 	body.classList.remove('lock');
 });
 
