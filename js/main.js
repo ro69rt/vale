605770c4c7cf61e7ff3e8fab6bafd2484ed9c11a
r69ert <frontend.robert@gmail.com>
2022-08-30 00:26:28 +0300

    fix bug

--- js/main.js
+++ js/main.js
@@ -6,39 +6,31 @@ const headerBack = document.querySelector(".header-block__back");
 const headerLogo = document.querySelector(".header-block__logo");
 const body = document.querySelector("body");
 
-const mediaQuery = window.matchMedia('(min-width: 768px)')
-
-function handleTabletChange(e) {
-	if (e.matches) {
+if (window.screen.availWidth > 670) {
 		searchInput.addEventListener('click', function (event) {
 			searchBlock.classList.add('active');
 		});
-	
-
 		document.onclick = function (e) {
 			if (e.target.className != "header-search__input") {
 				searchBlock.classList.remove('active');
 			};
 		};
-	}
 }
 
-searchInput.addEventListener('click', function (event) {
-	header.classList.add('header-modal__sticky');
-	headerModal.classList.add('active');
-	headerBack.classList.add('active');
-	headerLogo.style.display = "none";
-	body.classList.add('lock');
-});
-headerBack.addEventListener('click', function (event) {
-	header.classList.remove('header-modal__sticky');
-	headerModal.classList.remove('active');
-	headerBack.classList.remove('active');
-	headerLogo.style.display = "block";
-	body.classList.remove('lock');
-});
-
-
-mediaQuery.addListener(handleTabletChange)
-handleTabletChange(mediaQuery)
+if (window.screen.availWidth < 670) {
+	searchInput.addEventListener('click', function (event) {
+		header.classList.add('header-modal__sticky');
+		headerModal.classList.add('active');
+		headerBack.classList.add('active');
+		headerLogo.style.display = "none";
+		body.classList.add('lock');
+	});
+	headerBack.addEventListener('click', function (event) {
+		header.classList.remove('header-modal__sticky');
+		headerModal.classList.remove('active');
+		headerBack.classList.remove('active');
+		headerLogo.style.display = "block";
+		body.classList.remove('lock');
+	});
+}
 
